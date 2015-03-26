96a00e2dd119679c28cc32e56b07236e8d8f448b
Jesús Otero <jesusoterogomez@gmail.com>
2015-03-25 23:20:40 -0430

    Added pricing table calculations

--- js.js
+++ js.js
@@ -1,1 +1,20 @@
-// you can enter your JS here!
+
+// Room Selection Summary
+var rooms = $('.rooms_table_form select[name^="room"]');
+
+rooms.change(function (e) {
+	e.preventDefault();
+
+	var count = 0,
+		total = 0;
+
+	$.each(rooms, function(index, val) {
+		total = total + parseInt(val.value) * $(this).data('price');
+		count = count + parseInt(val.value);
+	});
+
+	$('.reservation_total').html('&euro;'+total.toFixed(2));
+	$('.reservation_quantity').html(count);
+});
+
+
