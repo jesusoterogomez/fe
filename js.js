d3c7d3ca0a9e33f352048334620f20527a7d618f
Jesús Otero <jesusoterogomez@gmail.com>
2015-03-26 23:11:06 -0430

    Added basic filtering support in booking table, replaced Euro symbols with CSS :pseudo-elements maintaining support for IE8

--- js.js
+++ js.js
@@ -1,10 +1,12 @@
 
 // Room Selection Summary
-var rooms = $('.rooms_table_form select[name^="room"]');
+var table = $(".rooms_table_form");
+var rooms = $(table).find('select[name^="room"]');
+var rows  = $(table).find('tbody .one_room');
+var cols  = $(table).find('thead > tr > th');
 
 rooms.change(function (e) {
 	e.preventDefault();
-
 	var count = 0,
 		total = 0;
 
@@ -17,4 +19,30 @@ rooms.change(function (e) {
 	$('.reservation_quantity').html(count);
 });
 
+cols.on('click', function (e) {
+	e.preventDefault();
+	var tbody = rows.parent();
+	var index = cols.index($(this)); // Index of Clicked Column
 
+	var colArray = rows.toArray(); // Rows of Clicked Column
+
+	$(rows).detach(); //remove row collection
+	colArray.sort(tableSort(index)); // apply sorting
+	
+	tbody.append(colArray); //append sorted row collection
+});
+
+function tableSort (index) {
+	return function (a,b){
+		var valA = $(a).find('td:eq('+index+')').html();
+		var	valB = $(b).find('td:eq('+index+')').html();
+
+		if (valA > valB){
+			return 1;
+		}
+		if (valA < valB){
+			return -1;
+		}
+		return 0;
+	}
+}
