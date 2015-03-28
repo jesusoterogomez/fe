0538cb169c7e2dd2f8ad7bdc405d26c02be43623
Jesús Otero <jesusoterogomez@gmail.com>
2015-03-27 23:29:16 -0430

    Completed sorting pricing table feature

--- js.js
+++ js.js
@@ -1,58 +1,69 @@
+var currency = '&euro;';
 
-// Room Selection Summary
-var table = $(".rooms_table_form");
-var rooms = $(table).find('select[name^="room"]');
-var rows  = $(table).find('tbody tr.one_room');
-var cols  = $(table).find('thead > tr > th');
+// Classes
+var roomQty    = 'select[name^="room"]';
+var roomPrice  = '.room_price';
 
-rooms.change(function (e) {
+// Room Table Selectors
+var table    = $(".rooms_table_form");
+var rows     = $(table).find('tbody tr.one_room');
+var cols     = $(table).find('thead > tr > th.sort');
+var subtotal = $(table).find('.summary_subtotal');
+var quantity = $(table).find('.summary_quantity');
+
+// Data Objects
+var price = {label:'price'};
+var sort  = {asc : 'asc', desc : 'desc'}; 
+
+/* Event on room quantity change */
+$(table).on('change', roomQty, function (e) {
 	e.preventDefault();
-	var count = 0,
-		total = 0;
+	var count = 0;
+	var total = 0;
 
-	$.each(rooms, function(index, val) {
-		total = total + parseInt(val.value) * $(this).data('price');
+	$.each(rows.find(roomQty), function(index, val) {
+		total = total + parseInt(val.value) * $(val).data(price.label);
 		count = count + parseInt(val.value);
 	});
 
-	$('.reservation_total').html('&euro;'+total.toFixed(2));
-	$('.reservation_quantity').html(count);
+	subtotal.html(currency+total.toFixed(2));
+	quantity.html(count);
 });
 
-cols.on('click', function (e) {
+/* Event on table header click */
+cols.click(function (e) {
 	e.preventDefault();
 	var tbody = rows.parent();
 	var index = cols.index($(this)); // Index of clicked column
 	var rowsArray = rows.toArray(); 
 
+	rowsArray.sort(tableSort(index)); // sort by column
+
 	$(rows).detach(); // Remove row collection
-	rowsArray.sort(tableSort(index)); 
 
-
-	$(cols).removeClass('sort');
-	$(this).addClass('sort');
-	// console.log(this);
-	tbody.append(rowsArray); // Append sorted row collection
+	if( ! $(this).hasClass(sort.asc) ){
+		tbody.append(rowsArray); // append sorted rows 
+		$(cols).removeClass(sort.asc+' '+sort.desc); // reset other columns' sorting
+		$(this).addClass(sort.asc).removeClass(sort.desc);
+	}
+	else{
+		tbody.append(rowsArray.reverse()); // append reversed rows
+		$(this).addClass(sort.desc).removeClass(sort.asc);
+	}
 });
 
+/* Sorting function */
 function tableSort (index) {
 	return function (a,b){
 		var valA = $(a).find('td:eq('+index+')').html();
 		var	valB = $(b).find('td:eq('+index+')').html();
-
-		//Check whether to sort by number or string
-		if($.isNumeric(valA) && $.isNumeric(valB))
-		{	
+		//Check whether to sort by number or string and apply sorting
+		if($.isNumeric(valA) && $.isNumeric(valB)){	
 			return valA - valB;
 		}
-		else
-		{
+		else{
 			return valA > valB ? 1 : -1;
-			// if (valA > valB){return  1;}
-			// else if (valA < valB){return -1;}
-			// return 0;	
 		}
 		return 0;
-
 	}
 }
