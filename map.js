c62c7e570e8681e9b69363144465c489c5445d69
Jesús Otero <jesusoterogomez@gmail.com>
2015-03-30 00:35:53 -0430

    Release Candidate - Code cleaned up

--- map.js
+++ map.js
@@ -1,11 +1,11 @@
-var map, placesList;
+var map, placesList, hotelMarker, request, google, callback;
 var placesCount = 6;
 
 // selectors
-var mapCanvas    = 'map-canvas';
+var mapCanvas    = 'map_canvas';
 var nearbyPlaces = '.nearby_places_list';
 var placeClass   = 'one_place';
-var placeTypes   = ['store','food'];
+var placeTypes   = ['store', 'food'];
 
 function initialize() {
   var paris = new google.maps.LatLng(48.8665735, 2.3551065);
@@ -14,12 +14,13 @@ function initialize() {
     center: paris,
     zoom: 17
   });
-  var hotelMarker = new google.maps.Marker({
+
+  hotelMarker = new google.maps.Marker({
     map: map,
     position: paris
   });
 
-  var request = {
+  request = {
     location: paris,
     radius: 500,
     types: placeTypes
@@ -31,18 +32,17 @@ function initialize() {
   service.nearbySearch(request, callback);
 }
 
-function callback(results, status, pagination) {
+function callback(results, status) {
   if (status != google.maps.places.PlacesServiceStatus.OK) {
     return;
-  } else {
-    createMarkers(results);
   }
+  createMarkers(results);
 }
 
 function createMarkers(places) {
   var bounds = new google.maps.LatLngBounds();
-
-  for (var i = 0; i < placesCount; i++) {
+  var i;
+  for (i = 0; i < placesCount; i++) {
     var image = {
       url: places[i].icon,
       size: new google.maps.Size(71, 71),
@@ -58,10 +58,10 @@ function createMarkers(places) {
       position: places[i].geometry.location
     });
 
-    placesList.innerHTML += '<li class="' +placeClass+ '">' +
-    '<img src="' + places[i].icon + '"/>' +
-    '<h4>' + places[i].name + '</h4>' +
-    '<address>' + places[i].vicinity + '</address></li>';
+    placesList.innerHTML += '<li class="' + placeClass + '">' +
+      '<img src="' + places[i].icon + '"/>' +
+      '<h4>' + places[i].name + '</h4>' +
+      '<address>' + places[i].vicinity + '</address></li>';
 
     bounds.extend(places[i].geometry.location);
   }
